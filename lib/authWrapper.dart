6dbfb8e7328a24148a65c0a127be735585a6e600
Gautam Kumar <gk39951@gmail.com>
2024-12-26 14:24:40 +0530

    use authwrapper for authentication

---
+++ lib/authWrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ticket_management/firebase_options.dart';
 import 'package:ticket_management/screens/login_screen.dart';
 import 'package:ticket_management/repos/ticket_repo.dart';
+import 'blocs/auth_bloc/auth_bloc_bloc.dart';
 import 'blocs/ticket_bloc/ticket_bloc_bloc.dart';
+import 'services/auth_services.dart';
+import 'services/user_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const MyApp());
 }
 
@@ -21,6 +23,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(
+            authService: FirebaseAuthService(),
+            userServices: UserServices(),
+          )..add(
+              // fires a bloc event at the time of init
+              AuthStarted(),
+            ), //
+        ),
         BlocProvider(create: (_) => TicketBlocBloc(TicketRepository())),
       ],
       child: const MaterialApp(
